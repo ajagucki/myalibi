591a837a3af89deed334260ea4b0ee4c020bcef8
armandod <armandod@188ecdda-2556-11de-9b37-5dcb32d212a7>
2009-01-06 18:59:56 +0000

    Adding default Eclipse Android project named 'Alibi'

---
+++ src/com/neutralspace/alibi/Alibi.java
@@ -0,0 +1,13 @@
+package com.neutralspace.alibi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Alibi extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
