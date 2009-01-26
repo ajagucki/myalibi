be12fcd22bd7ef203a8c341be4540a2ac0f6a1ed
armandod <armandod@188ecdda-2556-11de-9b37-5dcb32d212a7>
2009-01-26 04:07:32 +0000

    Renamed main launcher test case.

--- tests/src/com/neutralspace/alibi/StartEventTest.java
+++ tests/src/com/neutralspace/alibi/StartEventTest.java
@@ -6,10 +6,10 @@ import android.test.ActivityInstrumentationTestCase;
  * Make sure that the main launcher activity opens up properly, which will be
  * verified by {@link ActivityInstrumentationTestCase#testActivityTestCaseSetUpProperly}.
  */
-public class MainTest extends ActivityInstrumentationTestCase<Main> {
+public class StartEventTest extends ActivityInstrumentationTestCase<StartEvent> {
 
-    public MainTest() {
-        super("com.neutralspace.alibi", Main.class);
+    public StartEventTest() {
+        super("com.neutralspace.alibi", StartEvent.class);
     }
 
 }
