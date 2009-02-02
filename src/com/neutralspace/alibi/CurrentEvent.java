3450d8f03da0cb5e6221e7651043853b9bd60849
skyc <skyc@188ecdda-2556-11de-9b37-5dcb32d212a7>
2009-02-02 00:58:31 +0000

    Created current_event.xml and CurrentEvent.java

---
+++ src/com/neutralspace/alibi/CurrentEvent.java
@@ -0,0 +1,43 @@
+package com.neutralspace.alibi;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
+public class CurrentEvent extends AlibiActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.current_event);
+        
+        Button stopButton = (Button) findViewById(R.id.stop);
+        Button changeButton = (Button) findViewById(R.id.change);
+        Button cancelButton = (Button) findViewById(R.id.cancel);
+        
+        stopButton.setOnClickListener(new View.OnClickListener(){
+            
+            public void onClick(View view){
+                //  ...
+            }
+        });
+        
+        changeButton.setOnClickListener(new View.OnClickListener(){
+            
+            public void onClick(View view){
+                //  ...
+            }
+        });
+        
+        cancelButton.setOnClickListener(new View.OnClickListener(){
+            
+            public void onClick(View view){
+                // ...
+            }
+        });
+;
+    }
+
+}
