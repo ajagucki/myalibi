f8dd63210b55ec5d77db785f4dfee422d617b077
nertner <nertner@188ecdda-2556-11de-9b37-5dcb32d212a7>
2009-02-23 00:18:32 +0000

    The close button in the about screen now works.

--- src/com/neutralspace/alibi/AboutDialog.java
+++ src/com/neutralspace/alibi/AboutDialog.java
@@ -2,6 +2,9 @@ package com.neutralspace.alibi;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AboutDialog extends Dialog{
 
@@ -10,7 +13,43 @@ public class AboutDialog extends Dialog{
         this.setCancelable(true);
         this.setContentView(R.layout.about_alibi);
         this.setTitle("About myAlibi");
-        this.show();
 	}
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		Button creditsButton = (Button) findViewById(R.id.open_credits);
+		Button closeButton = (Button) findViewById(R.id.close_about);
+		
+		closeButton.setOnClickListener(new CloseAbout(this));
+		creditsButton.setOnClickListener(new OpenCredits(this));
+	}
+	
+	private class CloseAbout implements View.OnClickListener{
+		AboutDialog aboutDialog;
+		
+		public CloseAbout(AboutDialog aboutDialog) {
+			this.aboutDialog = aboutDialog;
+		}
+		
+		public void onClick(View v) {
+			aboutDialog.cancel();			
+		}
+		
+	}
+	
+	private class OpenCredits implements View.OnClickListener{
+		AboutDialog aboutDialog;
+		
+		public OpenCredits(AboutDialog aboutDialog) {
+			this.aboutDialog = aboutDialog;
+		}
 
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
 }
