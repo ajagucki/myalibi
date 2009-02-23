e31de92398d5d54eede2551a8ba01e126e046857
cairnsk <cairnsk@188ecdda-2556-11de-9b37-5dcb32d212a7>
2009-02-23 07:48:47 +0000

    Implemented location listener in GPS code. Needs testing on G1.

--- src/com/neutralspace/alibi/FindLocation.java
+++ src/com/neutralspace/alibi/FindLocation.java
@@ -4,20 +4,35 @@ import android.content.Context;
 import android.content.Intent;
 import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class FindLocation extends AlibiActivity {
-	private Location userLocation;
-
+public class FindLocation extends AlibiActivity implements LocationListener {
+//	private Location userLocation;
+	private LocationManager lm;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
 		setContentView(R.layout.retrieve_location);
+		Button skipButton = (Button) findViewById(R.id.skip_location);		
+		skipButton.setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				Log.i(Alibi.TAG, "event started without GPS location");
+			    Intent currentEventIntent = new Intent(v.getContext(), CurrentEvent.class);
+				startActivity(currentEventIntent);
+				finish();				
+			}			
+		});
+	
 
-        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+ 
         Criteria c = new Criteria();
         c.setAccuracy(Criteria.ACCURACY_FINE);
         c.setBearingRequired(false);
@@ -28,18 +43,27 @@ public class FindLocation extends AlibiActivity {
         String s = android.provider.Settings.System.getString
         (getContentResolver(), android.provider.Settings.System.LOCATION_PROVIDERS_ALLOWED);
         Log.d(Alibi.TAG, " avail GPS: " + s);
+
         
-        userLocation = lm.getLastKnownLocation(provider);
+        lm.requestLocationUpdates(provider, 0, 0, this);
+ //       userLocation = lm.getLastKnownLocation(provider);
         
         Log.d(Alibi.TAG, "Provider enabled status: " + lm.isProviderEnabled(provider));
        
-        if (userLocation == null) {
-        	userLocation = new Location("Unknown");
-        	Log.e(Alibi.TAG, "Provider '" + provider + "' was disabled.");
 
+        
+
+	}
+	
+	@Override
+	public void onLocationChanged(Location loc){
+		lm.removeUpdates(this);
+		
+        if (loc == null) {
+        	Log.e(Alibi.TAG, "Location is null. This is bad");
         } else {
-        	Log.i(Alibi.TAG, "Location set to " + userLocation.toString());
-        	((Alibi) getApplication()).getUserEventManager().getCurrentEvent().setLocation(userLocation);
+        	Log.i(Alibi.TAG, "Location set to " + loc.toString());
+            ((Alibi) getApplication()).getUserEventManager().getCurrentEvent().setLocation(loc);
         }
         
         Log.i(Alibi.TAG, "Calling current event");
@@ -47,6 +71,20 @@ public class FindLocation extends AlibiActivity {
 	    Intent currentEventIntent = new Intent(this.getBaseContext(), CurrentEvent.class);
 		startActivity(currentEventIntent);
 		finish();
+		
 	}
+
+	@Override
+	public void onProviderDisabled(String arg0) {				
+	}
+	@Override
+	public void onProviderEnabled(String provider) {				
+	}
+	@Override
+	public void onStatusChanged(String provider, int status,
+			Bundle extras) {				
+	}
+	
+	
 
 }
