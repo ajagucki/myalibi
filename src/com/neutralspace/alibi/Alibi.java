35b69ce89f1bdf841068964df0479e5529010e08
nertner <nertner@188ecdda-2556-11de-9b37-5dcb32d212a7>
2009-04-09 21:17:24 +0000

    Added Apache License

--- src/com/neutralspace/alibi/Alibi.java
+++ src/com/neutralspace/alibi/Alibi.java
@@ -1,3 +1,22 @@
+/************************************************************************
+Copyright 2009
+Adam DiCarlo, Armando Diaz-Jagucki, Kelsey Cairns,
+Nathan Ertner, Peter Welte, Sky Cunningham, and
+neutralSpace Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+************************************************************************/
+
 package com.neutralspace.alibi;
 
 import android.app.Application;
